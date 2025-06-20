17479d2559e7e433908b4442574be9f7f424e359
sainaidu <saimanikantavasireddy210@gmail.com>
2025-06-19 20:26:19 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('hello from jenkins backend on windows');
+});
+app.listen(port, () => {
+	console.log(`app running at http://localhost:${port}`);
+});
+
